9e71da0d970d6d048b9dcc21176d49e407ee5b6b
Oleksii Klymenchuk <oleksii.klymenchuk@gmail.com>
2021-02-14 14:17:16 +0200

    Code completely rewritten. - Added union for input unlimited number of coefficients; - Added added input validation for coefficients; - Added MAP for store coefficients and a discriminant; - Added method private List<Double> calculateRoots(Map<String, Double> coefficientsMap) which returns ArrayList with roots; - Modified final logic of displaying results. Leans on length List (number of roots).

--- src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part1.java
+++ src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part1.java
@@ -2,74 +2,78 @@ package com.shpp.p2p.cs.oklymenchuk.assignment2;
 
 import com.shpp.cs.a.console.TextProgram;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Solving a quadratic equation.
  */
-public class Assignment2Part1 extends TextProgram {
+public class Assignment2Part1<DISCRIMINANT> extends TextProgram {
 
-    // надо решать через МАПы!!! Это ФСЁ БАБУЙНЯ!!!
+    private static Map<String, Double> coefficientsMap = new HashMap<>();
 
-    private final static String SIGN_MINUS = "minus";
-    private final static String SIGN_PLUS = "plus";
-    private final static String COEFFICIENT_A = "a";
-    private final static String COEFFICIENT_B = "b";
-    private final static String COEFFICIENT_C = "c";
+    private static final String DISCRIMINANT = "Discriminant";
 
     /**
      * Main method.
      */
     public void run() {
 
-        double a = inputCoefficient(COEFFICIENT_A);
-        double b = inputCoefficient(COEFFICIENT_B);
-        double c = inputCoefficient(COEFFICIENT_C);
+        // find the size of the enum
+        Coefficient[] coefficients = Coefficient.values();
 
-        // for debug
-        // System.out.println(a + " " + b + " " + c);
-
-        // check a == 0
-        if (a == 0) {
-            System.out.println("It`s not a quadratic equation");
-            System.exit(-1);
+        // input coefficients
+        for (Coefficient c: coefficients) {
+            coefficientsMap.put(c.toString(), inputCoefficient(c.toString()));
         }
 
-        // find the discriminant
-        double theDiscriminant = (b * b) - (4 * a * c);
+        // calculate and put the discriminant in Map
+        coefficientsMap.put(DISCRIMINANT, calculateDiscriminant(coefficientsMap));
 
-        // for debug
-        //System.out.println("Discriminant is " + theDiscriminant);
+        /*// for debug only
+        for (Map.Entry<String, Double> entry : coefficientsMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
+        }*/
 
-        if (theDiscriminant < 0) {
+        // find all roots
+        List<Double> foundRoots = calculateRoots(coefficientsMap);
+
+        // show result
+        if (foundRoots.isEmpty()) {
             System.out.println("There are no real roots");
         }
 
-        if (theDiscriminant == 0) {
-            System.out.println("There is one root: " + theRoot(theDiscriminant, a, b, SIGN_MINUS));
+        if (foundRoots.size() == 1) {
+            System.out.println("There is one root: " + foundRoots.get(0));
         }
 
-        if (theDiscriminant > 0) {
-            System.out.println("There are two roots: " + theRoot(theDiscriminant, a, b, SIGN_MINUS)
-                    + " and " +
-                    theRoot(theDiscriminant, a, b, SIGN_PLUS));
+        if (foundRoots.size() == 2) {
+            System.out.println("There are two roots: " + foundRoots.get(0) + " and " + foundRoots.get(1));
         }
     }
 
-    /**
-     * Return a root of the quadratic equation
-     * @param discriminant
-     * @param a
-     * @param b
-     * @param sign
-     * @return
-     */
-    private double theRoot(double discriminant, double a, double b, String sign) {
-        if (sign.equals(SIGN_MINUS)) {
-            return (-b - Math.sqrt(discriminant)) / (2 * a);
-        } else {
-            return (-b + Math.sqrt(discriminant)) / (2 * a);
+    private List<Double> calculateRoots(Map<String, Double> coefficientsMap) {
+
+        List<Double> roots = new ArrayList<>();
+
+        if (coefficientsMap.get(DISCRIMINANT) > 0){
+
+            roots.add((-1 * coefficientsMap.get("b") + Math.sqrt(coefficientsMap.get(DISCRIMINANT))) /
+                    (2 * coefficientsMap.get("a")));
+
+            roots.add((-1 * coefficientsMap.get("b") - Math.sqrt(coefficientsMap.get(DISCRIMINANT))) /
+                    (2 * coefficientsMap.get("a")));
         }
+
+        if (coefficientsMap.get(DISCRIMINANT) == 0){
+            roots.add(- coefficientsMap.get("b") / (2 * coefficientsMap.get("a")));
+        }
+
+        return roots;
+    }
+
+    private double calculateDiscriminant(Map<String, Double> coefficientsMap){
+        return (coefficientsMap.get("b") * coefficientsMap.get("b")) -
+                (4 * coefficientsMap.get("a") * coefficientsMap.get("c"));
     }
 
     private double inputCoefficient(String coefficient){
@@ -84,4 +88,8 @@ public class Assignment2Part1 extends TextProgram {
             }
         }
     }
+}
+
+enum Coefficient{
+    a, b, c
 }
