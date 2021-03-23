84fd5e54931799c7e469db5685d0ca70a2de5876
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-03-23 12:46:25 +0200

    Added WINDOW field where show any orientation flag; Added properties for GLabel; Added method drawSignature(); Refactored some code.
    Need to:
    - add MAP for storing FLAG objects;
    - comments!

--- src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part4.java
+++ src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part4.java
@@ -14,19 +14,16 @@ public class Assignment2Part4 extends WindowProgram {
 	public static final int HEIGHT = 300;
 	public static final int WIDTH = 450;
 
-	private static final int VERTICAL_FLAG_OFFSET_X = WIDTH / 5;                                   	// x-offset from the left border for a vertical flag
-	private static final int VERTICAL_FLAG_OFFSET_Y = HEIGHT / 10;                                  // y-offset from the top border for a vertical flag
+	private static final int WINDOW_SHOWS_FLAG_OFFSET_X = WIDTH / 6;    // X of the start WINDOW
+	private static final int WINDOW_SHOWS_FLAG_OFFSET_Y = HEIGHT / (2 * 5);    // Y of the start WINDOW
 
-	private static final int VERTICAL_FLAG_SEGMENT_WIDTH = WIDTH / 5;                               // width of one vertical flag`s segment
-	private static final int VERTICAL_FLAG_SEGMENT_HEIGHT = HEIGHT - (2 * VERTICAL_FLAG_OFFSET_Y);  // height of one vertical flag`s segment
-
-	private static final int HORIZONTAL_FLAG_OFFSET_X = WIDTH / 5;
-	private static final int HORIZONTAL_FLAG_OFFSET_Y = HEIGHT / 5;
-
-	private static final int HORIZONTAL_FLAG_SEGMENT_WIDTH = WIDTH * 3 / 5;
-	private static final int HORIZONTAL_FLAG_SEGMENT_HEIGHT = HEIGHT / 5;
+	private static final int WINDOW_SHOWS_FLAG_WIDTH = WIDTH * 4 / 6;    // WINDOW WIDTH
+	private static final int WINDOW_SHOWS_FLAG_HEIGHT = HEIGHT * 4 / 5; // WINDOW HEIGHT
 
 	private static final String MESSAGE_FLAG_OF = "Flag of ";
+	private static final String LABEL_FONT = "SansSerif-bold-16";
+	private static final int LABEL_OFFSET_RIGHT_BORDER = 10;
+	private static final int LABEL_OFFSET_BOTTOM_BORDER = 10;
 
 	public void run() {
 		// для генерации случайных значений сделать мапу (ключ: номер, значение: объект?)
@@ -34,53 +31,76 @@ public class Assignment2Part4 extends WindowProgram {
 				new Color[]{Color.BLACK, Color.YELLOW, Color.RED},
 				true);
 
+		CountryFlag flagBurkinaFaso = new CountryFlag("Burkina - Faso",
+				new Color[]{Color.BLACK, Color.YELLOW, Color.RED, Color.CYAN},
+				true);
+
 		CountryFlag flagRussia = new CountryFlag("Russia Federation",
 				new Color[]{Color.BLACK, Color.BLUE, Color.RED},
 				false);
 
-		drawFlag(flagBelgium);
+		CountryFlag flagSpectre = new CountryFlag("Spectral Republic",
+				new Color[]{Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA},
+				false);
+		CountryFlag flagUkraine = new CountryFlag("Ukraine",
+				new Color[]{Color.BLUE, Color.YELLOW},
+				false);
+
+//		drawFlag(flagBelgium);
 //		drawFlag(flagRussia);
+//		drawFlag(flagBurkinaFaso);
+//		drawFlag(flagSpectre);
+		drawFlag(flagUkraine);
 
 	}
 
 	private void drawFlag(CountryFlag flag) {
 		if (flag.isVertical) {
 
-			int offsetX = VERTICAL_FLAG_OFFSET_X;
+			int offsetX = WINDOW_SHOWS_FLAG_OFFSET_X;
+			int segmentLength = WINDOW_SHOWS_FLAG_WIDTH / flag.flagColors.length;
 
 			for (int i = 0; i < flag.flagColors.length; i++) {
-				drawVerticalSegment(offsetX, flag.flagColors[i]);
-				offsetX = offsetX + VERTICAL_FLAG_SEGMENT_WIDTH;
+				drawVerticalSegment(offsetX, segmentLength, flag.flagColors[i]);
+				offsetX = offsetX + segmentLength;
 			}
 		} else {
 
-			int offsetY = HORIZONTAL_FLAG_OFFSET_Y;
+			int offsetY = WINDOW_SHOWS_FLAG_OFFSET_Y;
+			int segmentLength = WINDOW_SHOWS_FLAG_HEIGHT / flag.flagColors.length;
 
 			for (int i = 0; i < flag.flagColors.length; i++) {
-				drawHorizontalSegment(offsetY, flag.flagColors[i]);
-				offsetY = offsetY + HORIZONTAL_FLAG_SEGMENT_HEIGHT;
+				drawHorizontalSegment(offsetY, segmentLength, flag.flagColors[i]);
+				offsetY = offsetY + segmentLength;
 			}
 		}
-		// draw label !!! NEED TO REWRITE
-		GLabel country = new GLabel(MESSAGE_FLAG_OF + flag.countryName);
-		double x = getWidth() * 0.6;	// bullshit! need to push off the right border
-		double y = getHeight() * 0.97;	// and to push off the bottom border
-		country.setFont("SansSerif-bold-18");
-		add(country, x, y);
+
+		drawSignature(flag.countryName);            // draw country name
 	}
 
-	private void drawVerticalSegment(int offsetX, Color flagColor) {
-		GRect verticalSegment = new GRect(offsetX, VERTICAL_FLAG_OFFSET_Y, VERTICAL_FLAG_SEGMENT_WIDTH, VERTICAL_FLAG_SEGMENT_HEIGHT);
+	private void drawVerticalSegment(int offsetX, int segmentLength, Color flagColor) {
+		GRect verticalSegment = new GRect(offsetX, WINDOW_SHOWS_FLAG_OFFSET_Y,
+				segmentLength, WINDOW_SHOWS_FLAG_HEIGHT);
 		verticalSegment.setColor(flagColor);
 		verticalSegment.setFilled(true);
 		add(verticalSegment);
 	}
 
-	private void drawHorizontalSegment(int offsetY, Color flagColor) {
-		GRect segmentOfFlag = new GRect(HORIZONTAL_FLAG_OFFSET_X, offsetY, HORIZONTAL_FLAG_SEGMENT_WIDTH, HORIZONTAL_FLAG_SEGMENT_HEIGHT);
+	private void drawHorizontalSegment(int offsetY, int segmentLength, Color flagColor) {
+		GRect segmentOfFlag = new GRect(WINDOW_SHOWS_FLAG_OFFSET_X, offsetY,
+				WINDOW_SHOWS_FLAG_WIDTH, segmentLength);
 		segmentOfFlag.setColor(flagColor);
 		segmentOfFlag.setFilled(true);
 		add(segmentOfFlag);
+	}
+
+	private void drawSignature(String nameCountry) {
+		GLabel signature = new GLabel(MESSAGE_FLAG_OF + nameCountry);
+		signature.setFont(LABEL_FONT);
+		double x = (getWidth() - signature.getWidth()) - LABEL_OFFSET_RIGHT_BORDER;        // offset from the right border
+		double y = signature.getDescent() > LABEL_OFFSET_BOTTOM_BORDER ? getHeight() - LABEL_OFFSET_BOTTOM_BORDER :
+				getHeight() - LABEL_OFFSET_BOTTOM_BORDER;    // offset 10 pxs from the bottom border
+		add(signature, x, y);
 	}
 
 	class CountryFlag {
