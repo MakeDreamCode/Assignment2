499b29e7cfda11b61ccc7ecffd5f8a213952b336
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-03-23 15:39:26 +0200

    First version. Fully working. Need to add comments!

---
+++ src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part5.java
@@ -0,0 +1,55 @@
+package com.shpp.p2p.cs.oklymenchuk.assignment2;
+
+import acm.graphics.GRect;
+import com.shpp.cs.a.graphics.WindowProgram;
+
+import java.awt.*;
+
+public class Assignment2Part5 extends WindowProgram {
+	/* The number of rows and columns in the grid, respectively. */
+	private static final int NUM_ROWS = 5;
+	private static final int NUM_COLS = 6;
+
+	/* The width and height of each box. */
+	private static final double BOX_SIZE = 40;
+
+	/* The horizontal and vertical spacing between the boxes. */
+	private static final double BOX_SPACING = 10;
+
+	/* Bugfix for Window (toolbar uses 23 pixels) */
+	public static final int APPLICATION_HEIGHT = 300 + 23;
+	public static final int APPLICATION_WIDTH = 350;
+
+	public static final int HEIGHT = 300;
+	public static final int WIDTH = 350;
+
+	/* FRAME*/
+	private static final double WINDOW_SHOWS_BOXES_WIDTH = NUM_COLS * (BOX_SIZE + BOX_SPACING) - BOX_SPACING;
+	private static final double WINDOW_SHOWS_BOXES_HEIGHT = NUM_ROWS * (BOX_SIZE + BOX_SPACING) - BOX_SPACING;
+
+	/* Start position of Frame*/
+	private static final double WINDOW_SHOWS_BOXES_OFFSET_X = (WIDTH - WINDOW_SHOWS_BOXES_WIDTH) / 2;
+	private static final double WINDOW_SHOWS_BOXES_OFFSET_Y = (HEIGHT - WINDOW_SHOWS_BOXES_HEIGHT) / 2;
+
+	public void run(){
+
+		double offsetX = WINDOW_SHOWS_BOXES_OFFSET_X;
+		double offsetY = WINDOW_SHOWS_BOXES_OFFSET_Y;
+
+		for (int i = 0; i < NUM_ROWS ; i++) {
+			for (int j = 0; j < NUM_COLS; j++) {
+				drawBox(offsetX, offsetY);
+				offsetX = offsetX + BOX_SIZE + BOX_SPACING;
+			}
+			offsetX = WINDOW_SHOWS_BOXES_OFFSET_X;
+			offsetY = offsetY + BOX_SIZE + BOX_SPACING;
+		}
+	}
+
+	private void drawBox(double offsetX, double offsetY){
+		GRect box = new GRect(offsetX, offsetY, BOX_SIZE, BOX_SIZE);
+		box.setColor(Color.BLACK);
+		box.setFilled(true);
+		add(box);
+	}
+}
