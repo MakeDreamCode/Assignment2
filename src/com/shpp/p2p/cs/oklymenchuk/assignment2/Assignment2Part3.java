2cb031882b474444937a04a5120b5b8be85f9f1a
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-03-29 08:40:10 +0300

    Refactored code: - added printHeel(double x, double y); - added printToe(double x, double y).
    Fix formatting + comments.
    For deploy.

--- src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part3.java
+++ src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part3.java
@@ -5,31 +5,10 @@ import com.shpp.cs.a.graphics.WindowProgram;
 
 import java.awt.*;
 
+/**
+ * Task 3. Pawprint.
+ */
 public class Assignment2Part3 extends WindowProgram {
-
-	/* Constants controlling the relative positions of the
-	 * three toes to the upper-left corner of the pawprint.
-	 */
-	private static final double FIRST_TOE_OFFSET_X = 0;
-	private static final double FIRST_TOE_OFFSET_Y = 20;
-	private static final double SECOND_TOE_OFFSET_X = 30;
-	private static final double SECOND_TOE_OFFSET_Y = 0;
-	private static final double THIRD_TOE_OFFSET_X = 60;
-	private static final double THIRD_TOE_OFFSET_Y = 20;
-
-	/* The position of the heel relative to the upper-left
-	 * corner of the pawprint.
-	 */
-	private static final double HEEL_OFFSET_X = 20;
-	private static final double HEEL_OFFSET_Y = 40;
-
-	/* Each toe is an oval with this width and height. */
-	private static final double TOE_WIDTH = 20;
-	private static final double TOE_HEIGHT = 30;
-
-	/* The heel is an oval with this width and height. */
-	private static final double HEEL_WIDTH = 40;
-	private static final double HEEL_HEIGHT = 60;
 
 	/* The default width and height of the window. These constants will tell Java to
 	 * create a window whose size is *approximately* given by these dimensions. You should
@@ -38,7 +17,26 @@ public class Assignment2Part3 extends WindowProgram {
 	 */
 	public static final int APPLICATION_WIDTH = 270;
 	public static final int APPLICATION_HEIGHT = 220;
+	/* Constants controlling the relative positions of the three toes to the upper-left corner of the pawprint(). */
+	private static final double FIRST_TOE_OFFSET_X = 0;
+	private static final double FIRST_TOE_OFFSET_Y = 20;
+	private static final double SECOND_TOE_OFFSET_X = 30;
+	private static final double SECOND_TOE_OFFSET_Y = 0;
+	private static final double THIRD_TOE_OFFSET_X = 60;
+	private static final double THIRD_TOE_OFFSET_Y = 20;
+	/* The position of the heel relative to the upper-left corner of the pawprint(). */
+	private static final double HEEL_OFFSET_X = 20;
+	private static final double HEEL_OFFSET_Y = 40;
+	/* Each toe is an oval with this width and height. */
+	private static final double TOE_WIDTH = 20;
+	private static final double TOE_HEIGHT = 30;
+	/* The heel is an oval with this width and height. */
+	private static final double HEEL_WIDTH = 40;
+	private static final double HEEL_HEIGHT = 60;
 
+	/**
+	 * Main method.
+	 */
 	public void run() {
 		drawPawprint(20, 20);
 		drawPawprint(180, 70);
@@ -47,30 +45,39 @@ public class Assignment2Part3 extends WindowProgram {
 	/**
 	 * Draws a pawprint. The parameters should specify the upper-left corner of the
 	 * bounding box containing that pawprint.
-	 *
 	 * @param x The x coordinate of the upper-left corner of the bounding box for the pawprint.
 	 * @param y The y coordinate of the upper-left corner of the bounding box for the pawprint.
 	 */
 	private void drawPawprint(double x, double y) {
-		// here is your turn to type some code!!
-		GOval heel = new GOval(x + HEEL_OFFSET_X, y + HEEL_OFFSET_Y, HEEL_WIDTH, HEEL_HEIGHT);
+		/* print the heel */
+		printHeel(x + HEEL_OFFSET_X, y + HEEL_OFFSET_Y);
+		/* print three toes */
+		printToe(x + FIRST_TOE_OFFSET_X, y + FIRST_TOE_OFFSET_Y);
+		printToe(x + SECOND_TOE_OFFSET_X, y + SECOND_TOE_OFFSET_Y);
+		printToe(x + THIRD_TOE_OFFSET_X, y + THIRD_TOE_OFFSET_Y);
+	}
+
+	/**
+	 * Draw a heel of a paw.
+	 * @param x - The x coordinate of the upper-left corner of the bounding box for the heel.
+	 * @param y - The y coordinate of the upper-left corner of the bounding box for the heel.
+	 */
+	private void printHeel(double x, double y){
+		GOval heel = new GOval(x, y, HEEL_WIDTH, HEEL_HEIGHT);
 		heel.setFillColor(Color.BLACK);
 		heel.setFilled(true);
 		add(heel);
+	}
 
-		GOval firstToe = new GOval(x + FIRST_TOE_OFFSET_X, y + FIRST_TOE_OFFSET_Y, TOE_WIDTH, TOE_HEIGHT);
+	/**
+	 * Draw a toe of paw.
+	 * @param x - The x coordinate of the upper-left corner of the bounding box for the toe.
+	 * @param y - The y coordinate of the upper-left corner of the bounding box for the toe.
+	 */
+	private void printToe(double x, double y){
+		GOval firstToe = new GOval(x, y, TOE_WIDTH, TOE_HEIGHT);
 		firstToe.setFillColor(Color.BLACK);
 		firstToe.setFilled(true);
 		add(firstToe);
-
-		GOval secondToe = new GOval(x + SECOND_TOE_OFFSET_X, y + SECOND_TOE_OFFSET_Y, TOE_WIDTH, TOE_HEIGHT);
-		secondToe.setFillColor(Color.BLACK);
-		secondToe.setFilled(true);
-		add(secondToe);
-
-		GOval thirdToe = new GOval(x + THIRD_TOE_OFFSET_X, y + THIRD_TOE_OFFSET_Y, TOE_WIDTH, TOE_HEIGHT);
-		thirdToe.setFillColor(Color.BLACK);
-		thirdToe.setFilled(true);
-		add(thirdToe);
 	}
 }
