a7ffd468d63268e9f6baaaca624935f421dd28df
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-03-30 01:26:34 +0300

    Fix formatting + comments. For deploy.

--- src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part5.java
+++ src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part5.java
@@ -6,47 +6,52 @@ import com.shpp.cs.a.graphics.WindowProgram;
 import java.awt.*;
 
 public class Assignment2Part5 extends WindowProgram {
+	/* Bugfix for Window (toolbar uses 23 pixels) */
+	public static final int APPLICATION_HEIGHT = 350 + 23;
+	public static final int APPLICATION_WIDTH = 400;
 	/* The number of rows and columns in the grid, respectively. */
 	private static final int NUM_ROWS = 5;
 	private static final int NUM_COLS = 6;
-
 	/* The width and height of each box. */
 	private static final double BOX_SIZE = 40;
-
 	/* The horizontal and vertical spacing between the boxes. */
 	private static final double BOX_SPACING = 10;
+	/* Object for iterable */
+	private static final double ITERABLE = BOX_SIZE + BOX_SPACING;
+	/* FRAME */
+	/* FRAME WIDTH & HEIGHT */
+	private static final double FRAME_WIDTH = (NUM_COLS * ITERABLE) - BOX_SPACING;
+	private static final double FRAME_HEIGHT = (NUM_ROWS * ITERABLE) - BOX_SPACING;
+	/* x, y position for the FRAME*/
+	private static final double FRAME_OFFSET_X = (APPLICATION_WIDTH - FRAME_WIDTH) / 2;
+	private static final double FRAME_OFFSET_Y = (APPLICATION_HEIGHT - 23 - FRAME_HEIGHT) / 2;
 
-	/* Bugfix for Window (toolbar uses 23 pixels) */
-	public static final int APPLICATION_HEIGHT = 300 + 23;
-	public static final int APPLICATION_WIDTH = 350;
-
-	public static final int HEIGHT = 300;
-	public static final int WIDTH = 350;
-
-	/* FRAME*/
-	private static final double FRAME_WIDTH = NUM_COLS * (BOX_SIZE + BOX_SPACING) - BOX_SPACING;
-	private static final double FRAME_HEIGHT = NUM_ROWS * (BOX_SIZE + BOX_SPACING) - BOX_SPACING;
-
-	/* Start position of Frame*/
-	private static final double FRAME_OFFSET_X = (WIDTH - FRAME_WIDTH) / 2;
-	private static final double FRAME_OFFSET_Y = (HEIGHT - FRAME_HEIGHT) / 2;
-
-	public void run(){
-
+	/**
+	 * Main method.
+	 */
+	public void run() {
+		/* Initialize firsts coordinates */
 		double offsetX = FRAME_OFFSET_X;
 		double offsetY = FRAME_OFFSET_Y;
 
-		for (int i = 0; i < NUM_ROWS ; i++) {
+		for (int i = 0; i < NUM_ROWS; i++) {
 			for (int j = 0; j < NUM_COLS; j++) {
+				/* draw boxes at the row*/
 				drawBox(offsetX, offsetY);
-				offsetX = offsetX + BOX_SIZE + BOX_SPACING;
+				offsetX = offsetX + ITERABLE;
 			}
+			/* drop the offsetX to the start position + iterate offsetY */
 			offsetX = FRAME_OFFSET_X;
-			offsetY = offsetY + BOX_SIZE + BOX_SPACING;
+			offsetY = offsetY + ITERABLE;
 		}
 	}
 
-	private void drawBox(double offsetX, double offsetY){
+	/**
+	 * Draws a box.
+	 * @param offsetX - the x-coordinate of the upper left point of the box;
+	 * @param offsetY - the y-coordinate of the upper left point of the box;
+	 */
+	private void drawBox(double offsetX, double offsetY) {
 		GRect box = new GRect(offsetX, offsetY, BOX_SIZE, BOX_SIZE);
 		box.setColor(Color.BLACK);
 		box.setFilled(true);
