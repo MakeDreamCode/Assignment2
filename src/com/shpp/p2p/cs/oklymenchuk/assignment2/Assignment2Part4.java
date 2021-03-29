fc6e76f308bf3d79cb6692528b9f6b950aaab0a1
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-03-30 00:54:06 +0300

    Fix formatting + comments. For deploy.

--- src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part4.java
+++ src/com/shpp/p2p/cs/oklymenchuk/assignment2/Assignment2Part4.java
@@ -6,112 +6,102 @@ import com.shpp.cs.a.graphics.WindowProgram;
 
 import java.awt.*;
 
+/**
+ * Task 4. Tricolor flags.
+ */
 public class Assignment2Part4 extends WindowProgram {
 
 	public static final int APPLICATION_HEIGHT = 300 + 23; //bugfix for win (23 pixels for the topbar)
 	public static final int APPLICATION_WIDTH = 450;
+	/* for readable code only*/
+	public static final int HEIGHT = APPLICATION_HEIGHT - 23;
 
-	public static final int HEIGHT = 300;
-	public static final int WIDTH = 450;
-
-	private static final int WINDOW_SHOWS_FLAG_OFFSET_X = WIDTH / 6;    // X of the start WINDOW
-	private static final int WINDOW_SHOWS_FLAG_OFFSET_Y = HEIGHT / (2 * 5);    // Y of the start WINDOW
-
-	private static final int WINDOW_SHOWS_FLAG_WIDTH = WIDTH * 4 / 6;    // WINDOW WIDTH
-	private static final int WINDOW_SHOWS_FLAG_HEIGHT = HEIGHT * 4 / 5; // WINDOW HEIGHT
-
+	/* FRAME */
+	/* x, y position for the FRAME*/
+	private static final int FRAME_OFFSET_X = APPLICATION_WIDTH / 6;
+	private static final int FRAME_OFFSET_Y = HEIGHT / (2 * 5);
+	/* FRAME WIDTH & HEIGHT */
+	private static final int FRAME_WIDTH = APPLICATION_WIDTH * 4 / 6;
+	private static final int FRAME_HEIGHT = HEIGHT * 4 / 5;
+	/* Label properties */
 	private static final String MESSAGE_FLAG_OF = "Flag of ";
 	private static final String LABEL_FONT = "SansSerif-bold-16";
 	private static final int LABEL_OFFSET_RIGHT_BORDER = 10;
 	private static final int LABEL_OFFSET_BOTTOM_BORDER = 10;
 
+	/**
+	 * Main method.
+	 */
 	public void run() {
-		// для генерации случайных значений сделать мапу (ключ: номер, значение: объект?)
+		/* Create object Flag of Belgium */
 		CountryFlag flagBelgium = new CountryFlag("Belgium",
-				new Color[]{Color.BLACK, Color.YELLOW, Color.RED},
-				true);
-
-		CountryFlag flagBurkinaFaso = new CountryFlag("Burkina - Faso",
-				new Color[]{Color.BLACK, Color.YELLOW, Color.RED, Color.CYAN},
-				true);
-
-		CountryFlag flagRussia = new CountryFlag("Russia Federation",
-				new Color[]{Color.BLACK, Color.BLUE, Color.RED},
-				false);
-
-		CountryFlag flagSpectre = new CountryFlag("Spectral Republic",
-				new Color[]{Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA},
-				false);
-		CountryFlag flagUkraine = new CountryFlag("Ukraine",
-				new Color[]{Color.BLUE, Color.YELLOW},
-				false);
-
-//		drawFlag(flagBelgium);
-//		drawFlag(flagRussia);
-//		drawFlag(flagBurkinaFaso);
-//		drawFlag(flagSpectre);
-		drawFlag(flagUkraine);
-
+				new Color[]{Color.BLACK, Color.YELLOW, Color.RED});
+		/* draw the flag*/
+		drawFlag(flagBelgium);
 	}
 
+	/**
+	 * Draws a flag.
+	 *
+	 * @param flag - class CountryFlag.
+	 */
 	private void drawFlag(CountryFlag flag) {
-		if (flag.isVertical) {
+		/* calculates the X - start position + the segment`s length */
+		int offsetX = FRAME_OFFSET_X;
+		/* how many segments will be show (equals color number) */
+		int segmentLength = FRAME_WIDTH / flag.flagColors.length;
 
-			int offsetX = WINDOW_SHOWS_FLAG_OFFSET_X;
-			int segmentLength = WINDOW_SHOWS_FLAG_WIDTH / flag.flagColors.length;
-
-			for (int i = 0; i < flag.flagColors.length; i++) {
-				drawVerticalSegment(offsetX, segmentLength, flag.flagColors[i]);
-				offsetX = offsetX + segmentLength;
-			}
-		} else {
-
-			int offsetY = WINDOW_SHOWS_FLAG_OFFSET_Y;
-			int segmentLength = WINDOW_SHOWS_FLAG_HEIGHT / flag.flagColors.length;
-
-			for (int i = 0; i < flag.flagColors.length; i++) {
-				drawHorizontalSegment(offsetY, segmentLength, flag.flagColors[i]);
-				offsetY = offsetY + segmentLength;
-			}
+		/* fills FRAME with vertical segments */
+		for (int i = 0; i < flag.flagColors.length; i++) {
+			drawVerticalSegment(offsetX, segmentLength, flag.flagColors[i]);
+			/* calculates the next X (start position of the segment) */
+			offsetX = offsetX + segmentLength;
 		}
-
-		drawSignature(flag.countryName);            // draw country name
+		/* Draw a country name signature */
+		drawSignature(flag.countryName);
 	}
 
+	/**
+	 * Draw a segment fo flag.
+	 *
+	 * @param offsetX       - start x-position of the vertical segment;
+	 * @param segmentLength - length of the vertical segment;
+	 * @param flagColor     - array of Color objects.
+	 */
 	private void drawVerticalSegment(int offsetX, int segmentLength, Color flagColor) {
-		GRect verticalSegment = new GRect(offsetX, WINDOW_SHOWS_FLAG_OFFSET_Y,
-				segmentLength, WINDOW_SHOWS_FLAG_HEIGHT);
+		GRect verticalSegment = new GRect(offsetX, FRAME_OFFSET_Y,
+				segmentLength, FRAME_HEIGHT);
 		verticalSegment.setColor(flagColor);
 		verticalSegment.setFilled(true);
 		add(verticalSegment);
 	}
 
-	private void drawHorizontalSegment(int offsetY, int segmentLength, Color flagColor) {
-		GRect segmentOfFlag = new GRect(WINDOW_SHOWS_FLAG_OFFSET_X, offsetY,
-				WINDOW_SHOWS_FLAG_WIDTH, segmentLength);
-		segmentOfFlag.setColor(flagColor);
-		segmentOfFlag.setFilled(true);
-		add(segmentOfFlag);
-	}
-
+	/**
+	 * Draws a signature.
+	 *
+	 * @param nameCountry - string from CountryFlag class.
+	 */
 	private void drawSignature(String nameCountry) {
 		GLabel signature = new GLabel(MESSAGE_FLAG_OF + nameCountry);
 		signature.setFont(LABEL_FONT);
-		double x = (getWidth() - signature.getWidth()) - LABEL_OFFSET_RIGHT_BORDER;        // offset from the right border
+		/* calculates offset from the right border */
+		double x = (getWidth() - signature.getWidth()) - LABEL_OFFSET_RIGHT_BORDER;
+		/* calculates offset from the bottom border */
 		double y = signature.getDescent() > LABEL_OFFSET_BOTTOM_BORDER ? getHeight() - signature.getDescent() :
-				getHeight() - LABEL_OFFSET_BOTTOM_BORDER;    // offset 10 pxs from the bottom border
+				getHeight() - LABEL_OFFSET_BOTTOM_BORDER;
 		add(signature, x, y);
 	}
 
-	class CountryFlag {
+	/**
+	 * Class CountryFlag.
+	 */
+	static class CountryFlag {
 		String countryName;
 		Color[] flagColors;
-		boolean isVertical;
 
-		public CountryFlag(String countryName, Color[] flagColors, boolean isVertical) {
+		public CountryFlag(String countryName, Color[] flagColors) {
 			this.countryName = countryName;
 			this.flagColors = flagColors;
-			this.isVertical = isVertical;
 		}
 	}
 }
